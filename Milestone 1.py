82f792ca359f1ad46d7fd8c725a0fb5d0625aaf8
David Gan <166639546+kingbadger18@users.noreply.github.com>
2026-01-11 19:09:36 +0100

    Optimize and shorten the code

--- Milestone 1.py
+++ Milestone 1.py
@@ -5,161 +5,68 @@ import datetime
 #References : https://docs.python.org/2/library/datetime.html
 #References : https://docs.python.org/2/library/calendar.html
 
-def compute_total_days(s_m, s_d, s_y, e_m, e_d, e_y):
-    start  = datetime.date(s_y, s_m, s_d)
-    end = datetime.date(e_y, e_m, e_d)
-    total = end - start
-    return total.days + 1
-
-def compute_weekdays(s_m, s_d, s_y, e_m, e_d, e_y):
-    start  = datetime.date(s_y, s_m, s_d)
-    end = datetime.date(e_y, e_m, e_d)
-    total = (end - start).days
-    day_of_start = start.weekday()
-    day_of_end = end.weekday()
-    if day_of_start == 0 and day_of_end == 0: 
-        return int((total//7) * 2)
-    elif day_of_start == 0 and day_of_end == 1: 
-        return int((total//7) * 2)
-    elif day_of_start == 0 and day_of_end == 2: 
-        return int((total//7) * 2)
-    elif day_of_start == 0 and day_of_end == 3: 
-        return int((total//7) * 2)
-    elif day_of_start == 0 and day_of_end == 4: 
-        return int((total//7) * 2)
-    elif day_of_start == 0 and day_of_end == 5: 
-        return int(((total//7) * 2) + 1)
-    elif day_of_start == 0 and day_of_end == 6: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 1 and day_of_end == 0: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 1 and day_of_end == 1: 
-        return int((total//7) * 2)
-    elif day_of_start == 1 and day_of_end == 2: 
-        return int((total//7) * 2)
-    elif day_of_start == 1 and day_of_end == 3: 
-        return int((total//7) * 2)
-    elif day_of_start == 1 and day_of_end == 4: 
-        return int((total//7) * 2)
-    elif day_of_start == 1 and day_of_end == 5: 
-        return int(((total//7) * 2) + 1)
-    elif day_of_start == 1 and day_of_end == 6: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 2 and day_of_end == 0: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 2 and day_of_end == 1: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 2 and day_of_end == 2: 
-        return int((total//7) * 2)
-    elif day_of_start == 2 and day_of_end == 3: 
-        return int((total//7) * 2)
-    elif day_of_start == 2 and day_of_end == 4: 
-        return int((total//7) * 2)
-    elif day_of_start == 2 and day_of_end == 5: 
-        return int(((total//7) * 2) + 1)
-    elif day_of_start == 2 and day_of_end == 6: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 3 and day_of_end == 0: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 3 and day_of_end == 1: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 3 and day_of_end == 2: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 3 and day_of_end == 3: 
-        return int((total//7) * 2)
-    elif day_of_start == 3 and day_of_end == 4: 
-        return int((total//7) * 2)
-    elif day_of_start == 3 and day_of_end == 5: 
-        return int(((total//7) * 2) + 1)
-    elif day_of_start == 3 and day_of_end == 6: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 4 and day_of_end == 0: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 4 and day_of_end == 1: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 4 and day_of_end == 2: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 4 and day_of_end == 3: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 4 and day_of_end == 4: 
-        return int((total//7) * 2)
-    elif day_of_start == 4 and day_of_end == 5: 
-        return int(((total//7) * 2) + 1)
-    elif day_of_start == 4 and day_of_end == 6: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 5 and day_of_end == 0: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 5 and day_of_end == 1: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 5 and day_of_end == 2: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 5 and day_of_end == 3: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 5 and day_of_end == 4: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 5 and day_of_end == 5: 
-        return int(((total//7) * 2) + 1)
-    elif day_of_start == 5 and day_of_end == 6: 
-        return int(((total//7) * 2) + 2)
-    elif day_of_start == 6 and day_of_end == 0: 
-        return int(((total//7) * 2) + 1)
-    elif day_of_start == 6 and day_of_end == 1: 
-        return int(((total//7) * 2) + 1)
-    elif day_of_start == 6 and day_of_end == 2: 
-        return int(((total//7) * 2) + 1)
-    elif day_of_start == 6 and day_of_end == 3: 
-        return int(((total//7) * 2) + 1)
-    elif day_of_start == 6 and day_of_end == 4: 
-        return int(((total//7) * 2) + 1)
-    elif day_of_start == 6 and day_of_end == 5: 
-        return int(((total//7) * 2) + 2)
-    else: 
-        return int(((total//7) * 2) + 2)
-
-def compute_leap_years(s_y, e_y):
-    start1  = datetime.datetime(s_y, s_m, s_d)
-    end1 = datetime.datetime(e_y, e_m, e_d)
-    if calendar.isleap(s_y) == True and calendar.isleap(e_y) == True:
-        if start1 >= datetime.datetime(s_y, 3, 1):
-            if end1 <= datetime.datetime(e_y, 2, 28):
-                return calendar.leapdays(s_y+1,e_y)
-            else: 
-                return calendar.leapdays(s_y+1,e_y) + 1
-        else:
-            if end1 <= datetime.datetime(e_y, 2, 28):
-                return calendar.leapdays(s_y+1,e_y) + 1
-            else:
-                return calendar.leapdays(s_y+1,e_y) + 2
-    elif calendar.isleap(s_y) == False and calendar.isleap(e_y) == False :
-        return calendar.leapdays(s_y,e_y) 
-    elif calendar.isleap(s_y) == True and calendar.isleap(e_y) == False :
-        if start1 >= datetime.datetime(s_y, 3, 1):
-            return calendar.leapdays(s_y + 1,e_y)
-        else:
-            return calendar.leapdays(s_y + 1,e_y) + 1
-    elif calendar.isleap(s_y) == False and calendar.isleap(e_y) == True:
-        if end1 <= datetime.datetime(e_y, 2, 28):
-            return calendar.leapdays(s_y,e_y) 
-        else:
-            return calendar.leapdays(s_y,e_y) + 1
-if __name__ == '__main__':
-    s_m = int(input().strip())
-
-    s_d = int(input().strip())
-
-    s_y = int(input().strip())
+def compute_total_days(start_month, start_day, start_year, end_month, end_day, end_year):
     
-    e_m = int(input().strip())
+    #datetime.date turns a calendar date into a type that can support mathematical and relational operations.
+    start = datetime.date(start_year, start_month, start_day)
+    end = datetime.date(end_year, end_month, end_day)
+    total = end - start
+    total_days = total.days + 1
+        
+    return total_days
 
-    e_d = int(input().strip())
+def compute_weekdays(start_month, start_day, start_year, end_month, end_day, end_year):
+    
+    start = datetime.date(start_year, start_month, start_day)
+    end = datetime.date(end_year, end_month, end_day)  
+    total = end - start
+    total_days = total.days + 1
+    full_weeks = total_days//7
+    weekdays = full_weeks * 5
+    
+    #datetime.timedelta represents a duration of time. It can be days, hours, minutes etc.
+    #the .weekday() function generates a int 0-6 wherein 0 represents Monday, 1 represents Tuesday and so on
+    #until Sunday which equals to 6.
+    remaining_days = total_days % 7 
+    for i in range(remaining_days):
+        if (start + datetime.timedelta(days=full_weeks * 7 + i)).weekday() < 5:
+            weekdays += 1
+            
+    return weekdays
 
-    e_y = int(input().strip())
- 
-print ("Enter start month: Enter start day: Enter start year: Enter end month: Enter end day: Enter end year:")    
-print ("total days from start date to end date:" , compute_total_days(s_m, s_d, s_y, e_m, e_d, e_y))
-print("")
-print ("total additional days from leap years:", compute_leap_years(s_y,e_y) )
-print("")
-print ('total weekends:', compute_weekdays(s_m, s_d, s_y, e_m, e_d, e_y) )
-print("")
-print ("total days without weekends:", (compute_total_days(s_m, s_d, s_y, e_m, e_d, e_y) - compute_weekdays(s_m, s_d, s_y, e_m, e_d, e_y)))
+def compute_leap_days(start_month, start_day, start_year, end_month, end_day, end_year):
+    
+    start = datetime.date(start_year, start_month, start_day)
+    end = datetime.date(end_year, end_month, end_day)
+    
+    leap_days = 0
+    for year in range(start_year, end_year + 1):
+        if calendar.isleap(year):
+            if start <= datetime.date(year, 2, 29) <= end:
+                leap_days += 1
+    
+    return leap_days
+    
+if __name__ == '__main__':
+    start_month = int(input().strip())
+
+    start_day = int(input().strip())
+
+    start_year = int(input().strip())
+    
+    end_month = int(input().strip())
+
+    end_day = int(input().strip())
+
+    end_year = int(input().strip())
+
+    total_days = compute_total_days(start_month, start_day, start_year, end_month, end_day, end_year)
+    weekdays = compute_weekdays(start_month, start_day, start_year, end_month, end_day, end_year)
+    weekends = total_days - weekdays
+    leap_days = compute_leap_days(start_month, start_day, start_year, end_month, end_day, end_year)
+    
+    print ("Enter start month: Enter start day: Enter start year: Enter end month: Enter end day: Enter end year:")    
+    print(f"total days from start date to end date: {total_days}")
+    print(f"\ntotal additional days from leap years: {leap_days}")
+    print(f"\ntotal weekends: {weekends}") 
+    print(f"\ntotal days without weekends: {weekdays}")
